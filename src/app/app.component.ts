00bdeefb17711f155cdc82be70a7643b183abf78
Stephano <stephano.ramos.p@gmail.com>
2017-03-28 17:20:16 -0300

    content: Section 4, Templates and Components, Video 18. Let Us Start Building Our Application Part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,18 @@ import {Account} from './account/account';
 })
 export class AppComponent {
     private _accounts: Array<Account> = [
-        {id: 1, title: 'Bank Xyz', description: 'This is my main bank account.', balance: 5},
+        {
+            id: 1,
+            title: 'Bank Xyz',
+            description: 'This is my main bank account.',
+            balance: 501.2
+        },
         new Account(2, 'Bank Asd', 'My secret account.', 1024.10)
     ];
 
     private _nextId = 3;
 
-    private createAcc(titleEl: any, descEl: any, balEl: any) {
+    private createAcc(titleEl: any, descEl: any, balEl: any): void {
         this._accounts.push(new Account(this._nextId, titleEl.value, descEl.value, balEl.value));
         this._nextId++;
 
@@ -23,7 +28,7 @@ export class AppComponent {
         balEl.value = 0;
     }
 
-    private removeAcc(index: number) {
+    private removeAcc(index: number): void {
         this._accounts.splice(index, 1);
     }
 }
