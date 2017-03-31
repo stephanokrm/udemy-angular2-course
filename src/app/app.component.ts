8ba79bdc8bfe9399c3fce39b2f5a15b3608a072e
Stephano <stephano.ramos.p@gmail.com>
2017-03-30 22:38:22 -0300

    content: Section 4, Templates and Components, Video 23. ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import {Account} from './account/account';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    private _nextId = 3;
+    private _selected: Array<boolean> = [false, false];
     private _accounts: Array<Account> = [
         {
             id: 1,
@@ -14,13 +16,17 @@ export class AppComponent {
             description: 'This is my main bank account.',
             balance: 501.2
         },
-        new Account(2, 'Bank Asd', 'My secret account.', 1024.10)
+        {
+            id: 2,
+            title: 'yyy Bank',
+            description: null,
+            balance: 322
+        }
     ];
-
-    private _nextId = 3;
-
+    
     private createAcc(titleEl: any, descEl: any, balEl: any): void {
         this._accounts.push(new Account(this._nextId, titleEl.value, descEl.value, balEl.value));
+        this._selected.push(false);
         this._nextId++;
 
         titleEl.value = '';
@@ -30,5 +36,10 @@ export class AppComponent {
 
     private removeAcc(index: number): void {
         this._accounts.splice(index, 1);
+        this._selected.splice(index, 1);
+    }
+
+    private select(index: number): void {
+        this._selected[index] = !this._selected[index];
     }
 }
