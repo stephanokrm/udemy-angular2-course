66b5dcd841e8743841577932f3dcd6576b15f686
Stephano Ramos <stephano.ramos.p@gmail.com>
2017-04-06 22:52:17 -0300

    content: Section 4, Templates and Components, Video 27. Separating Components Part 2

--- src/app/account/account-list/account-list.component.ts
+++ src/app/account/account-list/account-list.component.ts
@@ -1,16 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {Account} from '../account';
 
 @Component({
     selector: 'app-account-list',
     templateUrl: './account-list.component.html',
     styleUrls: ['./account-list.component.css']
 })
-export class AccountListComponent implements OnInit {
+export class AccountListComponent {
+    private _selected: Array<boolean> = [false, false];
+    private _accounts: Array<Account> = [
+        {
+            id: 1,
+            title: 'Bank Xyz',
+            description: 'This is my main bank account.',
+            balance: 501.2
+        },
+        {
+            id: 2,
+            title: 'yyy Bank',
+            description: null,
+            balance: 322
+        }
+    ];
 
-    constructor() {
+    private removeAcc(index: number): void {
+        this._accounts.splice(index, 1);
+        this._selected.splice(index, 1);
     }
 
-    ngOnInit() {
+    private select(index: number): void {
+        this._selected[index] = !this._selected[index];
     }
-
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,15 @@ import {Account} from './account/account';
 })
 export class AppComponent {
     private _nextId = 3;
-    private _selected: Array<boolean> = [false, false];
-    private _accounts: Array<Account> = [
-        {
-            id: 1,
-            title: 'Bank Xyz',
-            description: 'This is my main bank account.',
-            balance: 501.2
-        },
-        {
-            id: 2,
-            title: 'yyy Bank',
-            description: null,
-            balance: 322
-        }
-    ];
 
     private createAcc(titleEl: any, descEl: any, balEl: any): void {
-        this._accounts.push(new Account(this._nextId, titleEl.value, descEl.value, balEl.value));
-        this._selected.push(false);
-        this._nextId++;
+        alert('Not implemented for now...');
+        // this._accounts.push(new Account(this._nextId, titleEl.value, descEl.value, balEl.value));
+        // this._selected.push(false);
+        // this._nextId++;
 
         titleEl.value = '';
         descEl.value = '';
         balEl.value = 0;
-    }
-
-    private removeAcc(index: number): void {
-        this._accounts.splice(index, 1);
-        this._selected.splice(index, 1);
-    }
-
-    private select(index: number): void {
-        this._selected[index] = !this._selected[index];
     }
 }
